60989f5d85b9fb4dc3b1ba72f3408f383b421f47
Hyesung0426 <141066717+Hyesung0426@users.noreply.github.com>
2024-07-26 09:28:01 +0900

    Update main.tf

--- S3/main.tf
+++ S3/main.tf
@@ -12,18 +12,18 @@ provider "aws" {
   profile = "terraform_user"
 }
 
-# AWS S3 Bucket 생성
+# AWS S3 Bucket 생성 (상태 파일 저장용)
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = "terraform-backend-sportlink"
+  bucket = "backend-test"
   tags = {
     Name = "terraform_state"
   }
   force_destroy = true
 }
 
-# DynamoDB 테이블 생성
+# DynamoDB 테이블 생성 (상태 파일 잠금용)
 resource "aws_dynamodb_table" "terraform_locks" {
-  name         = "terraform-backend-sportlink-locks"  # S3 버킷과 유사한 이름
+  name         = "test-dynamoDB"  # S3 버킷과 유사한 이름
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
 
@@ -35,4 +35,13 @@ resource "aws_dynamodb_table" "terraform_locks" {
   tags = {
     Name = "terraform-backend-sportlink-locks"
   }
+}
+
+# AWS S3 Bucket 생성 (이미지 저장용)
+resource "aws_s3_bucket" "image_storage" {
+  bucket = "test-image-storage"  # 버킷 이름을 유니크하게 설정
+  tags = {
+    Name = "image_storage"
+  }
+  force_destroy = true
 }
