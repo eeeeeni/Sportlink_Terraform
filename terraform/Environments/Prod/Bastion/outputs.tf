6268588d76d664137890f693d274dfc0e78d3ef9
Hyesung0426 <fkfkqkfk@naver.com>
2024-08-01 23:18:23 +0900

    복구5

---
+++ terraform/Environments/Prod/Bastion/outputs.tf
@@ -0,0 +1,14 @@
+output "BastionHost_AZ1_id" {
+  description = "The IDs of the BastionHost_AZ1_id"
+  value       = aws_instance.BastionHost_AZ1.id
+}
+
+output "BastionHost_AZ2_id" {
+  description = "The IDs of the BastionHost_AZ2_id"
+  value       = aws_instance.BastionHost_AZ2.id
+}
+
+output "BastionHost_SG_id" {
+  description = "The ID of the Bastion Host Security Group"
+  value       = aws_instance.BastionHost_SG.id
+}
--- terraform/Environments/Prod/RDS/outputs.tf
+++ terraform/Environments/Prod/RDS/outputs.tf
@@ -10,3 +10,13 @@ output "rds_master_password" {
   value       = jsondecode(aws_secretsmanager_secret_version.rds_master_password_version.secret_string).password
   sensitive   = true
 }
+
+output "rds_endpoint" {
+  description = "RDS instance endpoint"
+  value       = module.rds.this_db_instance_endpoint
+}
+
+output "db_instance_resource_id" {
+  description = "The ID of the RDS instance"
+  value       = module.rds.db_instance_resource_id
+}
