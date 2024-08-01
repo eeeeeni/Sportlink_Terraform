13941db37067d18b780808412738208dc6e46dd6
Hyesung0426 <141066717+Hyesung0426@users.noreply.github.com>
2024-08-01 11:15:15 +0900

    Delete Desktop/Sportlink_terraform-main2 directory

--- Desktop/Sportlink_terraform-main2/terraform/Environments/stage/Bastion/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "BastionHost_AZ1_id" {
-  description = "The IDs of the BastionHost_AZ1_id"
-  value       = aws_instance.BastionHost_AZ1.id
-}
-
-output "BastionHost_AZ2_id" {
-  description = "The IDs of the BastionHost_AZ2_id"
-  value       = aws_instance.BastionHost_AZ2.id
-}
